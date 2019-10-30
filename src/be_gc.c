9add2725bc5b2a051ed30fb132f21306a01e41a8
skiars <2269610337@qq.com>
2019-10-30 20:51:35 +0800

    native closure free in GC

--- src/be_gc.c
+++ src/be_gc.c
@@ -302,6 +302,7 @@ static void free_ntvclos(bvm *vm, bgcobject *obj)
         while (count--) {
             be_free(vm, *uv++, sizeof(bupval));
         }
+        be_free(vm, f, sizeof(bntvclos));
     }
 }
 
