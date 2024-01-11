dc897635e5d793190c9817267b9019e47c74ad3b
Stephan Hadinger <stephan.hadinger@gmail.com>
2024-01-11 22:52:05 +0100

    Add `debug.caller()`

--- src/be_debuglib.c
+++ src/be_debuglib.c
@@ -13,6 +13,7 @@
 #include "be_debug.h"
 #include "be_map.h"
 #include "be_vm.h"
+#include "be_exec.h"
 #include <string.h>
 
 #if BE_USE_DEBUG_MODULE
@@ -101,6 +102,26 @@ static int m_traceback(bvm *vm)
 {
     be_tracestack(vm);
     be_return_nil(vm);
+}
+
+static int m_caller(bvm *vm)
+{
+    int depth = 1;
+    if (be_top(vm) >= 1 && be_isint(vm, 1)) {
+        depth = be_toint(vm, 1);
+        if (depth < 0) {
+            depth = -depth;         /* take absolute value */
+        }
+    }
+    bcallframe *cf = (bcallframe*)be_stack_top(&vm->callstack) - depth;
+    bcallframe *base = be_stack_base(&vm->callstack);
+    if (cf >= base) {
+        bvalue *reg = be_incrtop(vm);
+        var_setval(reg, cf->func);
+        be_return(vm);
+    } else {
+        be_return_nil(vm);
+    }
 }
 
 #if BE_USE_DEBUG_HOOK
@@ -252,6 +273,7 @@ module debug (scope: global, depend: BE_USE_DEBUG_MODULE) {
     top, func(m_top)
     varname, func(m_varname), BE_DEBUG_VAR_INFO
     upvname, func(m_upvname), BE_DEBUG_VAR_INFO
+    caller, func(m_caller)
     // individual counters
     allocs, func(m_allocs)
     frees, func(m_frees)
