1175f6fdb8326a2e67ba3f05e616406c565eec4b
Stephan Hadinger <stephan.hadinger@gmail.com>
2026-08-20 12:05:27 +0000

    Harden VM and compiler against corrupt bytecode and hostile input

--- src/be_lexer.c
+++ src/be_lexer.c
@@ -247,6 +247,9 @@ static void tr_string(blexer *lexer)
             be_lexerror(lexer, "unfinished string");
             break;
         case '\\':
+            if (src >= end) {
+                be_lexerror(lexer, "invalid escape sequence");
+            }
             if (*src != 'u') {
                 switch (*src) {
                 case 'a': c = '\a'; break;
@@ -260,8 +263,17 @@ static void tr_string(blexer *lexer)
                 case '\'': c = '\''; break;
                 case '"': c = '"'; break;
                 case '?': c = '?'; break;
-                case 'x': c = read_hex(lexer, ++src); ++src; break;
+                case 'x':
+                    if (end - src < 3) {  /* need 'x' + 2 hex digits */
+                        be_lexerror(lexer, "invalid hexadecimal number");
+                    }
+                    c = read_hex(lexer, ++src);
+                    ++src;
+                    break;
                 default:
+                    if (end - src < 3) {  /* need 3 octal digits */
+                        be_lexerror(lexer, "invalid octal number");
+                    }
                     c = read_oct(lexer, src);
                     if (c != EOS) {
                         src += 2;
@@ -272,6 +284,9 @@ static void tr_string(blexer *lexer)
                 *dst++ = (char)c;
             } else {
                 /* unicode encoding, ex "\uF054" is equivalent to "\xEF\x81\x94"*/
+                if (end - src < 5) {  /* need 'u' + 4 hex digits */
+                    be_lexerror(lexer, "incorrect '\\u' encoding");
+                }
                 dst = be_load_unicode(dst, src + 1);
                 src += 5;
                 if (dst == NULL) {
@@ -300,6 +315,9 @@ static int skip_newline(blexer *lexer)
         next(lexer); /* skip "\n\r" or "\r\n" */
     }
     lexer->linenumber++;
+#if BE_USE_PREPROCESSOR
+    lexer->pp_at_line_start = btrue;
+#endif
     return lexer->reader.cursor;
 }
 
@@ -354,7 +372,9 @@ static btokentype scan_dot_real(blexer *lexer)
     if (is_digit(lgetc(lexer))) {
         match(lexer, is_digit);
         scan_realexp(lexer);
+        save_char(lexer, '\0');
         setreal(lexer, be_str2real(lexbuf(lexer), NULL));
+        --lexer->buf.len;  /* drop the trailing NUL we just added */
         return TokenReal;
     }
     return OptDot;
